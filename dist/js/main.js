57e1aa45553df515913b4bc4a553ec399efad61a
Gonzagam <canalblacktobio@gmail.com>
2023-07-06 22:31:51 -0300

    Navbar finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,18 @@
 const menuEl = document.getElementById("menu-btn");
+const sideMenuEl = document.getElementById("side-menu");
+let sideMenuOpen = false;
 let menuOpen = false;
 
 menuEl.addEventListener("click", () => {
   if (!menuOpen) {
     menuEl.classList.add("open");
     menuOpen = true;
+    sideMenuEl.classList.add("active");
+    sideMenuOpen = true;
   } else {
     menuEl.classList.remove("open");
     menuOpen = false;
+    sideMenuEl.classList.remove("active");
+    sideMenuOpen = false;
   }
 })
